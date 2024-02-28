dbbf6ec983093f3fe0510793dd07a97f81402a3d
wenquan <wenquan@harmonycloud.cn>
2024-02-28 18:02:18 +0800

    fix: 侧边栏菜单bug修复

--- config/routes.ts
+++ config/routes.ts
@@ -16,41 +16,37 @@ const routes: RouteProps[] = [
   },
   {
     path: '/',
-    component: '@/layouts',
-    wrappers: ['@/wrappers/auth'],
-    routes: [
-      {
-        path: '/',
-        redirect: '/home',
-      },
-      {
-        name: '首页',
-        path: 'home',
-        exact: true,
-        component: '@/pages/Home',
-      },
-      {
-        name: '账号管理',
-        path: 'account',
-        exact: true,
-        component: '@/pages/AccountManage',
-      },
-      {
-        name: '任务管理',
-        path: 'task',
-        exact: true,
-        component: '@/pages/TaskManage',
-      },
-      { path: '/404', component: '@/pages/Error/404' },
-      { path: '/noAuth', component: '@/pages/Error/noAuth' },
-      {
-        path: '/personCenter',
-        exact: true,
-        component: '@/pages/PersonCenter',
-      },
-      { path: '/*', component: '@/pages/Error/404' },
-    ],
+    redirect: '/home',
   },
+  {
+    name: '首页',
+    path: '/home',
+    exact: true,
+    wrappers: ['@/wrappers/auth'],
+    component: '@/pages/Home',
+  },
+  {
+    name: '账号管理',
+    path: '/account',
+    exact: true,
+    wrappers: ['@/wrappers/auth'],
+    component: '@/pages/AccountManage',
+  },
+  {
+    name: '任务管理',
+    path: '/task',
+    exact: true,
+    wrappers: ['@/wrappers/auth'],
+    component: '@/pages/TaskManage',
+  },
+  { path: '/404', component: '@/pages/Error/404' },
+  { path: '/noAuth', component: '@/pages/Error/noAuth' },
+  {
+    path: '/personCenter',
+    exact: true,
+    component: '@/pages/PersonCenter',
+  },
+  { path: '/*', component: '@/pages/Error/404' },
 ];
 
 export default routes;
